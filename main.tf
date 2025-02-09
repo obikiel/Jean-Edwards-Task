52481ded355c21836bffe8e19a78384069d3a933
obikiel <ezekielobiomachi1@gmail.com>
2025-02-09 19:26:17 +0100

    added files

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argocd"
+  create_namespace = true
+}
